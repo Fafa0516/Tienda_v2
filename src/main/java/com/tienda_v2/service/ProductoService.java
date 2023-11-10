6c96809977ce8d27aeaf90bf1735493a698573f1
Fabricio Jiménez <fabriciojc0516@gmail.com>
2023-11-10 16:39:31 -0600

    Semana 09

--- src/main/java/com/tienda_v2/service/ProductoService.java
+++ src/main/java/com/tienda_v2/service/ProductoService.java
@@ -4,17 +4,25 @@ import com.tienda_v2.domain.Producto;
 import java.util.List;
 
 public interface ProductoService {
-    
+
     //Se obtiene un array List con todas las productos de la tabla
     public List<Producto> getProductos(boolean activo);
-    
+
     //Se obtiene una Producto segun el Id pasado por parametro
     public Producto getProducto(Producto producto);
-            
+
     //Se actualiza una producto o se inserta una nueva... (Si no hay id es un insert)
     public void save(Producto producto);
-    
+
     //Se elimina una producto segun el id pasado
-    
     public void delete(Producto producto);
+
+    //Método para obtener un listado de producto filtrado por precio, ordenado por descripcion
+    public List<Producto> consultaQuery (double precioInf, double precioSup);
+    
+    //Método para obtener un listado de producto filtrado por precio, ordenado por descripcion
+    public List<Producto> consultaJPQL (double precioInf, double precioSup);
+    
+    //Método para obtener un listado de producto filtrado por precio, ordenado por descripcion
+    public List<Producto> consultaSQL (double precioInf, double precioSup);
 }
